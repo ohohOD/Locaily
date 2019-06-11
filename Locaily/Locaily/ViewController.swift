dbb27fa2ac56c624845d9a26de83efa02061a193
ohohOD <ksjj4791@gmail.com>
2019-06-11 17:01:10 +0900

    new project
    go!

---
+++ Locaily/Locaily/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Locaily
+//
+//  Created by SWUCOMPUTER on 6/11/19.
+//  Copyright © 2019 SWUCOMPUTER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
